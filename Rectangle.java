f7cc3c03c479fa5572af4088f14a79acf68e5dc9
Ishan <punjishanrenu@gmail.com>
2025-06-22 17:04:21 +0530

    changed visibility

--- Rectangle.java
+++ Rectangle.java
@@ -10,14 +10,20 @@ public class Rectangle{
         length=length;
         width=width;
     }
+    public int getLength(){
+        return length;
+    }
+    public int getWidth(){
+        return width;
+    }
 
 }
 class Test{
     public static void main(String[] args) {
         Rectangle r1=new Rectangle();
         Rectangle r2=new Rectangle(2,4);
-        System.out.println(r1.length+" "+r1.width);
-        System.out.println(r2.length+" "+r2.width);
+        System.out.println(r1.getLength()+" "+r1.getWidth());
+        System.out.println(r2.getLength()+" "+r2.getWidth());
     }
 
 }
